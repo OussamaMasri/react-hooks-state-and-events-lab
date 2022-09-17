019c826b851f69d72ee0fbde34c962db54ab7b87
Oussama Masri <oussamamasri16120@gmail.com>
2022-09-17 09:58:49 +0300

    Assignment Done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,26 @@
 import React from "react";
 
+
 function Item({ name, category }) {
+
+  const [isAdded, setIsAdded] = React.useState(false);
+
+  
+  function clickToCart(){
+    setIsAdded(function(isAdded){   
+      return !isAdded
+    })
+  }
+    
+  const cart = isAdded ? "in-cart" : "" ;
+  const addBtn = isAdded ? "remove" : "add" ;
+  const innerBtn = isAdded ? "Remove from Cart" : "Add to Cart" ;
+
   return (
-    <li className="">
+    <li className={cart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={addBtn} onClick={clickToCart}>{innerBtn}</button>
     </li>
   );
 }
